e3df8db600984c09c6c9017e0c15afbbb67d7cfc
Kshitij Garg <71349810+Kshitijgarg1902@users.noreply.github.com>
2025-11-01 01:08:13 +0530

    Change server port from 5000 to 1234

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import app from './server';
 import * as types from './types';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1234;
 
 app.listen(PORT, () => {
   console.log('Server Running on PORT:', PORT);
